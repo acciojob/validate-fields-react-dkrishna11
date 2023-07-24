b203d6aa84f6616d2d20f39a4796777c4117508d
Krishna_D <dkrishna051997@gmail.com>
2023-07-24 13:37:16 +0530

    submitting code-Mon Jul 24 2023 13:37:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [user, setUser]=useState({names:"", password:""})
+  let [error, setError]=useState("");
+
+
+  function login(){
+
+    if(names==="" || password==="")
+      setError("Both username and password are required")
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      Username:<input type="text" onChange={(e)=>setUser({...user, names:e.target.value})}/>
+      Password:<input type="password" onChange={(e)=>setUser({...user, password:e.target.value})}/>
+      {
+        error && <p id="errorMessage">{error}</p>
+      }
+      <button onClick={login}>Login</button>
     </div>
   )
 }
