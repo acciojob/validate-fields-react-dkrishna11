4e3226fb6c8e9021446a9c7872fa666ae13f0e54
Krishna_D <dkrishna051997@gmail.com>
2023-07-24 13:41:48 +0530

    submitting code-Mon Jul 24 2023 13:41:46 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-
-import React, {useState} from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  let [user, setUser]=useState({names:"", password:""})
-  let [error, setError]=useState("");
+  let [user, setUser] = useState({ names: "", password: "" });
+  let [error, setError] = useState("");
 
-
-  function login(){
-
-    if(user.names==="" || user.password==="")
-      setError("Both username and password are required")
-    else setError("")
+  function login() {
+    if (user.names === "" || user.password === "")
+      setError("Both username and password are required.");
+    else setError("");
   }
   return (
     <div>
-      Username:<input type="text" onChange={(e)=>setUser({...user, names:e.target.value})}/>
-      Password:<input type="password" onChange={(e)=>setUser({...user, password:e.target.value})}/>
-      {
-        error && <p id="errorMessage">{error}</p>
-      }
+      Username:
+      <input
+        type="text"
+        onChange={(e) => setUser({ ...user, names: e.target.value })}
+      />
+      Password:
+      <input
+        type="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      {error && <p id="errorMessage">{error}</p>}
       <button onClick={login}>Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
